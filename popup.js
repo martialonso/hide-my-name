6b4d60931900c1b7460a22817528ebc408fe7086
Marti Alonso <martialonso11@gmail.com>
2020-12-20 16:51:21 +0100

    Add link to options in popup

--- popup.js
+++ popup.js
@@ -7,3 +7,11 @@ chrome.storage.local.get(['active'], function(items) {
 active.addEventListener('click', function() {
 	chrome.storage.local.set({active: activeCheckbox.checked}, null);
 });
+
+document.getElementById('options').addEventListener('click', function() {
+	if (chrome.runtime.openOptionsPage) {
+		chrome.runtime.openOptionsPage();
+	} else {
+		window.open(chrome.runtime.getURL('options.html'));
+	}
+});
