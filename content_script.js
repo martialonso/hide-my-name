0f2f4c4855590cbd1f7162d8ff989a9f474af0db
Marti Alonso <martialonso11@gmail.com>
2020-12-20 15:11:22 +0100

    Add active logic and fix replace logic bugs

--- content_script.js
+++ content_script.js
@@ -1,32 +1,35 @@
 chrome.storage.local.get({
+	active: false,
     pages: [],
 	toHide: [],
 	replacement: []
   }, function(items) {
-	var pages = items.pages;
-	var toHide = items.toHide;
-	var replacement = items.replacement;
-	var currentUrl = location.toString();
-	for (var i = 0; i < pages.length; i++) {
-		if (pages[i] != '' && currentUrl.includes(pages[i])) {
-			for (var j = 0; j < toHide.length; j++) {
-				hideStuff(toHide[j], replacement[j]);
+	if (items.active) {
+		var pages = items.pages;
+		var toHide = items.toHide;
+		var replacement = items.replacement;
+		var currentUrl = location.toString();
+		for (var i = 0; i < pages.length; i++) {
+			if (pages[i] != '' && currentUrl.includes(pages[i])) {
+				for (var j = 0; j < toHide.length; j++) {
+					hideStuff(toHide[j], replacement[j]);
+				}
 			}
 		}
 	}
 });
 
 function hideStuff(toHide, replacement) {
-	var elements = document.evaluate('//*[contains(text(),"' + toHide + '")]', document, null, XPathResult.ANY_TYPE);
-	var element = elements.iterateNext();
-	var elem = [];
-	while (element) {
-		elem.push(element);
-		element = elements.iterateNext();
-	}
-
-	for (var i = 0; i < elem.length; i++) {
-		var e = elem[i];
-		e.innerText = e.innerText.replace(toHide, replacement);
+	var n;
+	var walk = document.createTreeWalker(document, NodeFilter.SHOW_TEXT, null, false);
+	while(n = walk.nextNode()) {
+		n.nodeValue = n.nodeValue.replaceCaseInsensitive(toHide, replacement);
 	}
 }
+
+String.prototype.replaceCaseInsensitive = function(strReplace, strWith) {
+    // See http://stackoverflow.com/a/3561711/556609
+    var esc = strReplace.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+    var reg = new RegExp(esc, 'ig');
+    return this.replace(reg, strWith);
+};
